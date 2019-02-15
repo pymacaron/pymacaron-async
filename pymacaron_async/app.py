fa1da33ebe77625802057cdf804659827a0659fe
Erwan Lemonnier <erwan@lemonnier.se>
2019-02-15 16:17:30 +0100

    Added support for scoutapp monitoring of background jobs

--- pymacaron_async/app.py
+++ pymacaron_async/app.py
@@ -1,5 +1,7 @@
 import logging
 from celery import Celery
+from pymacaron import get_config
+from pymacaron.utils import get_app_name
 
 
 log = logging.getLogger(__name__)
@@ -7,6 +9,21 @@ log = logging.getLogger(__name__)
 
 app = Celery('tasks')
 
+
+# Enable Scoutapp monitoring, if available
+conf = get_config()
+if hasattr(conf, 'scout_key'):
+    import scout_apm.celery
+    from scout_apm.api import Config
+
+    Config.set(
+        key=conf.scout_key,
+        name=get_app_name(),
+        monitor=True
+    )
+
+    scout_apm.celery.install()
+# END OF scoutapp support
 
 app.conf.update(
     task_serializer='json',
