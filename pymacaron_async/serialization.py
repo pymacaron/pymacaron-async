6acd4c0136171e1eda89864c23f7c3b3c9403de8
Erwan Lemonnier <erwan@lemonnier.se>
2022-01-14 15:19:45 +0100

    Use PymacaronBaseModel as introduced in pymacaron v2

--- pymacaron_async/serialization.py
+++ pymacaron_async/serialization.py
@@ -1,6 +1,6 @@
 from pymacaron.log import pymlogger
-from pymacaron_core.models import PyMacaronModel
-from pymacaron_core.models import get_model
+from pymacaron.model import PymacaronBaseModel
+from pymacaron import apipool
 
 
 log = pymlogger(__name__)
@@ -12,11 +12,12 @@ def model_to_task_arg(o):
     unchanged. If it's a list, serialize its items in the same way.
     """
 
-    if isinstance(o, PyMacaronModel):
+    if isinstance(o, PymacaronBaseModel):
         # It's a PyMacaron model, let's serialize it to json and attach a magic marker to it
         j = o.to_json()
         j['__pymacaron_model_name'] = o.get_model_name()
-        log.debug("Serialized PyMacaronModel %s to celery task arg" % j['__pymacaron_model_name'])
+        j['__pymacaron_api_name'] = o.get_model_api()
+        log.debug(f"Serialized PymacaronBaseModel {j['__pymacaron_model_name']} to celery task arg")
         return j
     elif type(o) is list:
         return [model_to_task_arg(oo) for oo in o]
@@ -28,9 +29,11 @@ def task_arg_to_model(o):
     """The opposite of model_to_tupple"""
     if type(o) is dict and '__pymacaron_model_name' in o:
         model_name = o['__pymacaron_model_name']
-        log.debug("Deserialized celery task arg to PyMacaronModel: %s" % model_name)
+        api_name = o['__pymacaron_api_name']
+        log.debug(f"Deserialized celery task arg to PymacaronBaseModel: {model_name}")
         del o['__pymacaron_model_name']
-        return get_model(model_name).from_json(o)
+        del o['__pymacaron_api_name']
+        return apipool.get_model(api_name).json_to_model(model_name, o)
     elif type(o) is list:
         return [task_arg_to_model(oo) for oo in o]
     else:
